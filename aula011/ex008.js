f7c95f021a657d7a0f7e80a16ea73786637ad18c
Arthur de Carvalho Miranda <104659615+arthurmiranda01@users.noreply.github.com>
2022-07-01 19:47:29 -0300

    ex

---
+++ aula011/ex008.js
@@ -0,0 +1,7 @@
+
+var vel = 80
+console.log(`a velocidade do seu carro é ${vel}Km/h`)
+if (vel > 60) { //condição simples
+    console.log(`Você passou o limite de velocidade, MULTADO`)
+}
+console.log(`Dirija com cuidado`)
