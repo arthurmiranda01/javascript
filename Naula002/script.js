6ad17fc6f737a150bf7889f10f1b4998ef77c9ee
Arthur de Carvalho Miranda <104659615+arthurmiranda01@users.noreply.github.com>
2022-09-06 15:35:31 -0300

    /

---
+++ Naula002/script.js
@@ -0,0 +1,14 @@
+let x = 10;
+let y = 5;
+console.log(x != y)
+
+let w = "10";
+let z = 10;
+console.log(w === z); 
+
+let preco = 40.3;
+if(preco > 0 && preco < 45) {
+    console.log('Está barato')
+} else {
+    console.log('Está caro')
+}
