dd9def1df49dfd00ba3e1c0b9d197b8dd5ed4aa7
Arthur de Carvalho Miranda <104659615+arthurmiranda01@users.noreply.github.com>
2022-09-08 11:58:54 -0300

    mais uma atividade js

--- Naula006/script.js
+++ Naula006/script.js
@@ -1,1 +1,7 @@
- 
+let carros = ['BMW', 'Ferrari', 'Mercedes'];
+console.log(carros[1])
+
+carros.push('Volvo')
+console.log(`Total de carros: ${carros.length}`)
+carros[1] = 'Audi'
+console.log(carros)
