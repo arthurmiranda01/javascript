2637b13f27d88194ad333d38bdae5380ea444173
Arthur de Carvalho Miranda <104659615+arthurmiranda01@users.noreply.github.com>
2022-10-03 13:16:49 -0300

    12

--- naula012/script.js
+++ naula012/script.js
@@ -1,21 +1,5 @@
-let timer; //criei a variável timer fora porque o let se fosse criado ali dentro da função não poderia ser usada em outra função
-
-function comecar(){
-    timer = setInterval(showTime, 1000); // ai aqui eu do valor pro timer
+function rodar() {
+    setTimeout(function(){
+        document.querySelector('.demo').innerHTML = 'Rodou!';
+    }, 2000);
 }
-function parar(){
-    clearInterval(timer); // e aqui eu limpo
-}
-
-
-
-function showTime() {
-    let hora = new Date();
-    let h = hora.getHours();
-    let m = hora.getMinutes();
-    let s = hora.getSeconds();
-    let txt = h+':'+m+':'+s;
-
-    document.querySelector('.demo').innerHTML = txt;
-}
-
